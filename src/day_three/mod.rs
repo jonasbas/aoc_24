61c316880c0c339bfcb0af82b256127b0d327b89
Jonas Bastisch <jonas.bastisch@gmail.com>
2024-12-03 10:40:37 +0100

    day three, part two

--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -19,3 +19,38 @@ pub fn part_one() {
 
     println!("* Part one: {}", result);
 }
+
+pub fn part_two() {
+    let content = fs::read_to_string("src/day_three/input.txt").unwrap();
+
+    let all_reg = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").expect("Regex error.");
+    let reg = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Regex error.");
+
+    let mut is_enabled = true;
+
+    let result: i32 = all_reg
+        .find_iter(&content)
+        .map(|x| {
+            let s = x.as_str();
+            if s.starts_with("mul") {
+                if is_enabled {
+                    let capture = reg.captures(s).unwrap();
+                    let one = capture[1].parse::<i32>().unwrap();
+                    let two = capture[2].parse::<i32>().unwrap();
+
+                    return one * two;
+                } else {
+                    return 0;
+                }
+            } else if s.starts_with("don't()") {
+                is_enabled = false;
+                return 0;
+            } else {
+                is_enabled = true;
+                return 0;
+            }
+        })
+        .sum();
+
+    println!("* Part two: {}", result);
+}
